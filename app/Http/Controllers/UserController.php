53522d88c99dec7a2c7c9eaed6ef79f9c8625650
Sofia <sofia.g.per@gmail.com>
2022-01-28 09:51:33 +0300

    Прописана валидация регистрации пользователя

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function signup(Request $request) {
         // dd($request->all());
 
-        //сохранение данных в БД
         $userData = $request->all();
 
+        //создание своего валидатора
+        $validator = Validator::make($userData, [
+            // | разделяет несколько правил валидации
+            'email' => 'required|unique:users|email:rfc,dns,filter',
+            'name' => 'required',
+            'password' => 'required',
+            'message' => 'required',
+        ]);
+
+        //валидация данных формы
+        //если данные заполнены не корректно
+        if ( $validator->fails()) {
+            return redirect( route('signup-page') )
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        //сохранение данных в БД
         $user = new User();
         $user->name = $userData['name'];
         $user->email = $userData['email'];
@@ -20,6 +38,8 @@ class UserController extends Controller
         $user->contacts = $userData['message'];
 
         $user->save();
+
+        return redirect(route('main-page'));
 
     }
 }
--- resources/views/sign-up.blade.php
+++ resources/views/sign-up.blade.php
@@ -4,29 +4,39 @@
 
 @section('page-content')
     <x-nav></x-nav>
-    <form class="form container form--invalid" action="{{ route('signup') }}" method="post" autocomplete="off"> <!-- form
-        --invalid -->
+    <form class="form container @if( $errors->any() ) {{ 'form--invalid' }} @endif" 
+        action="{{ route('signup') }}" 
+        method="post" 
+        autocomplete="off"> 
         @csrf
         <h2>Регистрация нового аккаунта</h2>
-        <div class="form__item"> <!-- form__item--invalid -->
+        <div class="form__item @error('email')form__item--invalid @enderror"> 
         <label for="email">E-mail <sup>*</sup></label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail">
-        <span class="form__error">Введите e-mail</span>
+        <input id="email" type="text" name="email" placeholder="Введите e-mail" value="{{ old('email') }}">
+        @error('email')
+            <span class="form__error">Введите e-mail</span>
+        @enderror
         </div>
-        <div class="form__item">
+        <div class="form__item @error('password')form__item--invalid @enderror">
         <label for="password">Пароль <sup>*</sup></label>
-        <input id="password" type="password" name="password" placeholder="Введите пароль">
-        <span class="form__error">Введите пароль</span>
+        <input id="password" type="password" name="password" placeholder="Введите пароль" value="{{ old('password') }}">
+        @error('password')
+            <span class="form__error">Введите пароль</span>
+        @enderror
         </div>
-        <div class="form__item">
+        <div class="form__item @error('name')form__item--invalid @enderror">
         <label for="name">Имя <sup>*</sup></label>
-        <input id="name" type="text" name="name" placeholder="Введите имя">
-        <span class="form__error">Введите имя</span>
+        <input id="name" type="text" name="name" placeholder="Введите имя" value="{{ old('name') }}">
+        @error('name')
+            <span class="form__error">Введите имя</span>
+        @enderror
         </div>
-        <div class="form__item">
+        <div class="form__item @error('message')form__item--invalid @enderror">
         <label for="message">Контактные данные <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите как с вами связаться"></textarea>
-        <span class="form__error">Напишите как с вами связаться</span>
+        <textarea id="message" name="message" placeholder="Напишите как с вами связаться">{{ old('message') }}</textarea>
+        @error('message')
+            <span class="form__error">Напишите как с вами связаться</span>
+        @enderror
         </div>
         <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
         <button type="submit" class="button">Зарегистрироваться</button>
