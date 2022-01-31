0e278ec8fb93c7e674f99711ea819a6d34d4c65b
Sofia <sofia.g.per@gmail.com>
2022-01-31 12:04:37 +0300

    Прописана валидация и сохранения лота в LotController и шаблоне add-lot.blade.php

--- app/Http/Controllers/LotController.php
+++ app/Http/Controllers/LotController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Category;
 use App\Models\Lot;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LotController extends Controller
 {
@@ -30,7 +32,35 @@ class LotController extends Controller
     }
 
     public function addlot(Request $request) {
-        
-        dd($request->all());
+
+        $lotData = $request->except('_token');
+        $validator = Validator::make($lotData, [
+            'lot-name'=>'required',
+            'category'=>'required',
+            'message'=>'required',
+            'lot-image'=>'required|mimes:png,jpeg',
+            'lot-rate'=>'required|min:1',
+            'lot-step'=>'required|min:1',
+            'lot-date'=>'required|date|after:tomorrow',
+        ]);
+
+        if ( $validator->fails() ) {
+            return redirect( route('addlot-page') )
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $lot = new Lot();
+        $lot->title = request('lot-name');
+        $lot->category_id = request('category');
+        $lot->author_id = Auth::user()->id;
+        $lot->description = request('message');
+        $lot->price = request('lot-rate');
+        $lot->bet_step = request('lot-step');
+        $lot->end_date = request('lot-date');
+        $lot->url = request()->file('lot-image')->store('img/lots');
+        $lot->save();
+
+        return redirect( route('lot-page', ['id' => $lot->id]) );
     }
 }
--- resources/views/add-lot.blade.php
+++ resources/views/add-lot.blade.php
@@ -14,57 +14,58 @@
 
 @section('page-content')
     <x-nav></x-nav>
-    <form class="form form--add-lot container form--invalid" action="{{route('addlot')}}" method="post"> <!-- form--invalid -->
+    <form class="form form--add-lot container @if( $errors->any() ) {{ 'form--invalid' }} @endif" 
+        action="{{route('addlot')}}" 
+        method="post"
+        enctype="multipart/form-data"> 
         @csrf
         <h2>Добавление лота</h2>
         <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+        <div class="form__item @error('lot-name') form__item--invalid @enderror">
             <label for="lot-name">Наименование <sup>*</sup></label>
-            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота">
+            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value="{{ @old('lot-name') }}">
             <span class="form__error">Введите наименование лота</span>
         </div>
-        <div class="form__item">
+        <div class="form__item @error('category') form__item--invalid @enderror">
             <label for="category">Категория <sup>*</sup></label>
             <select id="category" name="category">
-            <option>Выберите категорию</option>
-            <option>Доски и лыжи</option>
-            <option>Крепления</option>
-            <option>Ботинки</option>
-            <option>Одежда</option>
-            <option>Инструменты</option>
-            <option>Разное</option>
+                <option @if( !@old('category') ) {{ 'selected' }} @endif disabled>Выберите категорию</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}" @if(@old('category') == $category->id)  {{ 'selected' }} @endif>{{ $category->title }}</option>    
+                @endforeach
             </select>
             <span class="form__error">Выберите категорию</span>
         </div>
         </div>
-        <div class="form__item form__item--wide">
+        <div class="form__item form__item--wide  @error('message') form__item--invalid @enderror">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
+        <textarea id="message" name="message" placeholder="Напишите описание лота">{{ @old('message') }}</textarea>
         <span class="form__error">Напишите описание лота</span>
         </div>
-        <div class="form__item form__item--file">
+        <div class="form__item form__item--file  @error('lot-image') form__item--invalid @enderror">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
-            <input class="visually-hidden" type="file" id="lot-img" value="">
+            <input class="visually-hidden" type="file" id="lot-img" name="lot-image" value=" {{ @old('lot-image') }} ">
             <label for="lot-img">
-            Добавить
+                Добавить
             </label>
+            <span class="form__error">Прикрепите файл в формате jpeg или png</span>
         </div>
         </div>
         <div class="form__container-three">
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small  @error('lot-rate') form__item--invalid @enderror">
             <label for="lot-rate">Начальная цена <sup>*</sup></label>
-            <input id="lot-rate" type="text" name="lot-rate" placeholder="0">
+            <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="{{ @old('lot-rate') }}">
             <span class="form__error">Введите начальную цену</span>
         </div>
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small  @error('lot-step') form__item--invalid @enderror">
             <label for="lot-step">Шаг ставки <sup>*</sup></label>
-            <input id="lot-step" type="text" name="lot-step" placeholder="0">
+            <input id="lot-step" type="text" name="lot-step" placeholder="0" value="{{ @old('lot-step') }}">
             <span class="form__error">Введите шаг ставки</span>
         </div>
-        <div class="form__item">
+        <div class="form__item  @error('lot-date') form__item--invalid @enderror">
             <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
-            <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+            <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="{{ @old('lot-date') }}">
             <span class="form__error">Введите дату завершения торгов</span>
         </div>
         </div>
